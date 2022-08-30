4336e5978d4133b875c6d239431b4878e0d0ea2a
belyantseva <sandrabel@gmail.com>
2022-08-30 23:53:49 +0300

    BAA 2

---
+++ BAA 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Первое : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе : ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+Console.WriteLine("a > b");
+}
+else if (a < b)
+{
+Console.WriteLine("a < b");
+}
+else
+{
+Console.WriteLine("a = b");
+}
