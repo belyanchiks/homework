3f21f5038a60266ba2046e2a35c1783ab0fbc1bb
belyantseva <sandrabel@gmail.com>
2022-08-31 21:45:58 +0300

    BAA 82

--- BAA 8/Program.cs
+++ BAA 8/Program.cs
@@ -1,11 +1,5 @@
-﻿Console.Write("Введите число 1: ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Введите число 2: ");
-int b = int.Parse(Console.ReadLine());
-for (int i = a; i <= b; i++)
-{
-    if (i % 2 == 0) 
-    {
-        Console.Write(i + " ");
-    }
-}
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 2; i <= n; i += 2)
+    Console.WriteLine(i);
