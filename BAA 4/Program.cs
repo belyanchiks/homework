d4d6e63fb22aa0d8410e0affc07b33d6ce8cb593
belyantseva <sandrabel@gmail.com>
2022-08-31 20:44:58 +0300

    BAA4

---
+++ BAA 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine("Nubmer Max = " + a); 
+}
+else if (b > c)
+{
+    Console.WriteLine("Number Max = " + b); 
+}
+else
+{
+    Console.WriteLine("Number Max = " + c);
+}
+                    
