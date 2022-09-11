fedb560883a5952f7f4dd9414e89fd3548a02407
belyantseva <sandrabel@gmail.com>
2022-09-11 15:41:55 +0300

    BAA 29

---
+++ BAA 29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+int Read (string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int [] Numb  = new int[8];
+int i = 0 ;
+ while (i < Numb.Length)
+ {
+ int NumberA = Read("Введите число: ");
+ Numb[i] = NumberA;
+ i++;
+ }
+ Console.WriteLine("Ваш массив: ");
+ i = 0;
+ while (i < Numb.Length)
+ {
+    Console.Write(Numb[i]+"   ");
+ i++;
+ }
