ff3c77266de2c880f40c779672e37c7718d514b0
belyantseva <sandrabel@gmail.com>
2022-09-14 20:09:10 +0300

    BAA 35

---
+++ BAA 35/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] arr( int size , int minA , int maxA )
+{
+    int[] array = new int[size];
+    Random Numb = new Random();
+    for(int i = 0 ; i < size ; i++)
+    {
+        array[i] = Numb.Next(minA , maxA +1);
+    }
+    return array;
+}
+int countRange(int [] array)
+{
+    int count = 0;
+    for ( int i = 0 ; i < array.Length ; i++ )
+    {
+        if (array[i] > 9 && array[i] < 100 )
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] Number = arr(123 , 0 , 150);
+int counter = countRange(Number);
+Console.WriteLine(string.Join(",  ",Number));
+Console.WriteLine($"Они лежат в отрезке [10,99] - {counter}");
