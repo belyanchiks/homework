4e59118b3d9c15207559ab591768b6e5b08353b0
belyantseva <sandrabel@gmail.com>
2022-09-21 19:50:46 +0300

    BAA 47

---
+++ BAA 47/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.NextDouble() * 30 - 10;
+            matrix[i, j] = Math.Round(matrix[i, j], 1);
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Введите количество строк ");
+int n = ReadNumber("Введите количество столбцов ");
+double[,] matr = new double[m, n];
+PrintMatrix(matr);
