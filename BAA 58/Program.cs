4fc1796345f67063e06c1278a3c85525343fbbca
belyantseva <sandrabel@gmail.com>
2022-09-26 19:27:17 +0300

    BAA 58

---
+++ BAA 58/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+void GetMatrix(int[,] matrix)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(1, 15);
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] DoubleMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            matrix3[i, j] = matrix1[i, j] * matrix2[i, j];
+            Console.Write($"{matrix3[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return matrix3;
+}
+
+Console.Write("Введите количество строк: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] matrix1 = new int[i, j];
+int[,] matrix2 = new int[i, j];
+Console.WriteLine("Ваша первая матрица:");
+GetMatrix(matrix1);
+Console.WriteLine("Ваша вторая матрица:");
+GetMatrix(matrix2);
+Console.WriteLine("Произведение двух матриц:");
+DoubleMatrix(matrix1,matrix2);
