7a305688c75d050775c9cdc2df64bbdfae6f2987
belyantseva <sandrabel@gmail.com>
2022-09-28 19:15:09 +0300

    BAA 64

---
+++ BAA 64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers1toN(int n)
+{
+    if (n <= 0) return;
+    else
+    {
+        Console.Write(n + " ");
+        PrintNumbers1toN(n-1);
+    }
+}
+
+int number = ReadNumber("Введите N: ");
+PrintNumbers1toN(number);
